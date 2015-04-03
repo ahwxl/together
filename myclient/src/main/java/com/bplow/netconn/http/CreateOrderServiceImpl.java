b2a829525e47753f9d1da1e16fe2ad69102868fc
ahwxl <javawxl@126.com>
2015-04-03 19:41:21 +0800

    form

--- myclient/src/main/java/com/bplow/netconn/http/CreateOrderServiceImpl.java
+++ myclient/src/main/java/com/bplow/netconn/http/CreateOrderServiceImpl.java
@@ -15,19 +15,31 @@ import org.apache.http.HeaderIterator;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
 import org.apache.http.cookie.Cookie;
+import org.apache.http.cookie.CookieSpec;
+import org.apache.http.cookie.CookieSpecProvider;
+import org.apache.http.cookie.MalformedCookieException;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.apache.http.impl.cookie.BestMatchSpecFactory;
+import org.apache.http.impl.cookie.BrowserCompatSpec;
+import org.apache.http.impl.cookie.BrowserCompatSpecFactory;
+import org.apache.http.cookie.CookieOrigin;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,9 +58,34 @@ public class CreateOrderServiceImpl {
 	
 	public void createOrder(){
 		
+		CookieSpecProvider easySpecProvider = new CookieSpecProvider() {
+			public CookieSpec create(HttpContext context) {
+				return new BrowserCompatSpec() {
+					@Override
+					public void validate(Cookie cookie, CookieOrigin origin)
+							throws MalformedCookieException {
+						// Oh, I am easy
+					}
+				};
+			}
+
+		};
+		RequestConfig requestConfig = RequestConfig.custom()
+				.setCookieSpec("easy").setSocketTimeout(10000)
+				.setConnectTimeout(10000).build();
+		
+		Registry<CookieSpecProvider> r = RegistryBuilder
+				.<CookieSpecProvider> create()
+				.register(CookieSpecs.BEST_MATCH, new BestMatchSpecFactory())
+				.register(CookieSpecs.BROWSER_COMPATIBILITY,
+				new BrowserCompatSpecFactory())
+				.register("easy", easySpecProvider).build();
+		
 		LaxRedirectStrategy redirectStrategy = new LaxRedirectStrategy();
 		BasicCookieStore cookieStore = new BasicCookieStore();
 		CloseableHttpClient httpclient = HttpClients.custom().setUserAgent(USER_AGENT)
+				.setDefaultCookieSpecRegistry(r)
+				.setDefaultRequestConfig(requestConfig)
 				.setRedirectStrategy(redirectStrategy)
                 .setDefaultCookieStore(cookieStore)
                 .build();
@@ -115,6 +152,18 @@ public class CreateOrderServiceImpl {
         List<URI> redirectLocations = null;
         
         HttpPost httpPost = new HttpPost(createUrlStable);
+        
+        
+        httpPost.setHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+        		httpPost.setHeader("Accept-Encoding", "gzip, deflate");
+        		httpPost.setHeader("Accept-Language", "zh-CN,zh;q=0.8");
+        		httpPost.setHeader("Cache-Control", "max-age=0");
+        		httpPost.setHeader("Connection", "keep-alive");
+        		httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded");
+        		httpPost.setHeader(
+        				"User-Agent",
+        				"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36");
+        
         List <NameValuePair> nvps = new ArrayList <NameValuePair>();
         
         /*Map map = processIo.parseParam();
@@ -132,16 +181,16 @@ public class CreateOrderServiceImpl {
 			CloseableHttpResponse response2 = httpclient.execute(httpPost,context);
 			
 			// 获取所有的重定向位置
-			redirectLocations = context.getRedirectLocations();
-			HttpEntity entity2 = response2.getEntity();
 			
+			HttpEntity entity2 = response2.getEntity();
+			redirectLocations = context.getRedirectLocations();
 			if(redirectLocations!=null){
 				for(URI uri : redirectLocations){
 					System.out.println("|\nv\n" + uri.toASCIIString());
 				}
 			}
 			
-			System.out.println("Login form get: " + response2.getStatusLine());
+			System.out.println("第一次post请求status:" + response2.getStatusLine());
 			
 			Header head [] =response2.getAllHeaders();
 			for(Header hed : head){
@@ -165,7 +214,20 @@ public class CreateOrderServiceImpl {
         
         
         try {
+        	StringEntity strentity2 = new StringEntity(str,
+                    ContentType.create("plain/text", Consts.UTF_8));
+            strentity.setChunked(true);
+            httpPost.setEntity(strentity2);
 			CloseableHttpResponse response3 = httpclient.execute(httpPost,context);
+			
+			logger.info("第二次post请求status：{}",response3.getStatusLine());
+			processIo.printToConsole(response3.getEntity().getContent());
+			redirectLocations = context.getRedirectLocations();
+			if(redirectLocations!=null){
+				for(URI uri : redirectLocations){
+					System.out.println("|\nv\n" + uri.toASCIIString());
+				}
+			}
 		} catch (ClientProtocolException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
