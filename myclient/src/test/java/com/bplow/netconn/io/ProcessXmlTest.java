aaa3c1999ce564a1a1d223485bf8de5e46e3193e
ahwxl <javawxl@126.com>
2015-04-08 20:52:44 +0800

    tijiao

--- myclient/src/test/java/com/bplow/netconn/io/ProcessXmlTest.java
+++ myclient/src/test/java/com/bplow/netconn/io/ProcessXmlTest.java
@@ -4,15 +4,23 @@ import java.io.IOException;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 
-
+@ContextConfiguration(locations = { "/applicationContext.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
 public class ProcessXmlTest {
 	
-	public static ProcessXmlService processXml ;
+	@Autowired
+	private ProcessXmlService processXml ;
 	
 	@BeforeClass
 	public static void setUp() throws Exception {
-		processXml = new ProcessXmlService();
+		//processXml = new ProcessXmlService();
 
 	}
 	
