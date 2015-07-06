e6a1fa55e3250aa6a9de9090338a1041026e3393
ahwxl <javawxl@126.com>
2015-07-06 13:18:16 +0800

    init

---
+++ myclient/src/test/java/com/bplow/netconn/dbplay/dao/UserDaoTest.java
@@ -0,0 +1,29 @@
+package com.bplow.netconn.dbplay.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
+import com.bplow.netconn.dbplay.dao.UserDao;
+
+
+@ContextConfiguration(locations = { "/applicationContext.xml","/test-datasource.xml","/applicationContext-myclient.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
+public class UserDaoTest {
+	
+	@Autowired
+	@Qualifier("userDaoTest")
+	private UserDao userDao;
+	
+	@Test
+	public void inserUserTest(){
+		String sql = " insert into user(id,name) values(3,'张三')";
+		userDao.inserUser(sql);
+	}
+
+}
